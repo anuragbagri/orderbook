05f983088174a647511481c214b926d0e3abcb37
anurag bagri <anurag123bagri@gmail.com>
2025-09-22 10:42:38 +0530

    -

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,28 @@
+use actix_web::{
+    delete, get, post, web::{self, post}, App, HttpServer, Responder
+};
+
+#[actix_web::main]
+
+async  fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service()
+        .service()
+    })
+    .bind(("127.0.0.1", 3000))?
+    .run()
+    .await
+}
+
+
+#[get("/depth")]
+
+
+#[post("/order")]
+
+
+#[delete("/delete")]
+
+
 
